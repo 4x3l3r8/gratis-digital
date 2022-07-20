9c5d131c6168f55512e7f47752fd4a8778c498d5
Daniel Adesanya <dcadesanya@sidmach.com>
2022-07-20 09:36:56 +0100

    Counter init: coming soon page

---
+++ src/components/Counter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div className="grid grid-cols-4 grid-rows-1 gap-2 mx-24 md:mx-32">
+      <div className="flex flex-col justify-center py-3 bg-white rounded-md">
+        <h4 className="mx-auto text-2xl font-medium text-center text-black uppercase md:text-4xl md:font-bold">7</h4>
+        <p className="mx-auto">Days</p>
+      </div>
+      <div className="flex flex-col justify-center py-3 bg-white rounded-md">
+        <h4 className="mx-auto text-2xl font-medium text-center text-black uppercase md:text-4xl md:font-bold">22</h4>
+        <p className="mx-auto">Hours</p>
+      </div>
+      <div className="flex flex-col justify-center py-3 bg-white rounded-md">
+        <h4 className="mx-auto text-2xl font-medium text-center text-black uppercase md:text-4xl md:font-bold">13</h4>
+        <p className="mx-auto">Minutes</p>
+      </div>
+      <div className="flex flex-col justify-center py-3 bg-white rounded-md">
+        <h4 className="mx-auto text-2xl font-medium text-center text-black uppercase md:text-4xl md:font-bold">42</h4>
+        <p className="mx-auto">Seconds</p>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import Nav from '../components/Nav';
+import React from "react";
+import Nav from "../components/Nav";
+import Counter from "../components/Counter";
 
-const ComingSoon = () =>{
+const ComingSoon = () => {
   return (
     <div className="coming-soon items-center h-full bg-[#ef34ab]">
-      <Nav/>
-      <div className="flex items-center w-full">
-      <h1 className="text-2xl md:text-4xl font-medium md:font-bold text-center">Something awesome is coming soon</h1>
+      <Nav />
+      <div className="flex flex-col items-center w-full my-14">
+        <div className="flex flex-col w-4/5 space-y-6 md:w-2/4">
+          <h1 className="text-2xl font-medium text-center text-white uppercase mx-14 md:text-4xl md:font-bold">Something awesome is coming soon</h1>
+          <span className="text-center text-white md:mx-14">
+            Your all-in-one affiliate marketing tracking software track, automate and optimize your campaigns.
+          </span>
+          <Counter />
+        </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default ComingSoon;
